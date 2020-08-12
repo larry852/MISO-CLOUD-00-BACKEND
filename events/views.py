944562920c9aecffa2aedd84ada552273313c04b
larry852 <larryportocarrero@gmail.com>
2020-08-12 01:08:34 -0500

    :construction: save user request on event

--- events/views.py
+++ events/views.py
@@ -9,6 +9,9 @@ class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
     def get_queryset(self):
         user = self.request.user
         return self.queryset.filter(user=user)
