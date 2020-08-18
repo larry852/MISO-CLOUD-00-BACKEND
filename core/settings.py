f866f5aafae288002e6cbc4e7fbaefd6887b0168
larry852 <larryportocarrero@gmail.com>
2020-08-18 00:04:09 -0500

    :construction: Update timezone, and validate dates

--- core/settings.py
+++ core/settings.py
@@ -116,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-co'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -158,5 +158,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Setup CORS
 CORS_ORIGIN_ALLOW_ALL = True
-
-LANGUAGE_CODE = 'es-co'
--- events/serializers.py
+++ events/serializers.py
@@ -4,6 +4,13 @@ from .models import Event
 
 
 class EventSerializer(serializers.ModelSerializer):
+
+    def validate(self, data):
+        if data['event_initial_date'] > data['event_final_date']:
+            raise serializers.ValidationError(
+                {"event_initial_date": "Fecha de inicio está después de la fecha final."})
+        return data
+
     class Meta:
         model = Event
         exclude = ('user', 'datetime', )
